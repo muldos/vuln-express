ad69a0b1037ebc8c1911140943c7bbb6a8fde96b
muldos <davidro@jfrog.com>
2022-12-14 15:17:02 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// static assets directory
+app.use(express.static('public'));
+
+// index page, this callback contains code that can be exploited for CVE-2022-29078 
+app.get('/', function(req, res) {
+  if (!req.query.hasOwnProperty('id')){
+    req.query.id = 'Stranger';
+  }
+  res.render('pages/index',req.query);
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
